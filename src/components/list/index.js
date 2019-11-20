3fbaf7537e968a3ec6a1dbe33ada411de7ba29a2
xiongshuang <xsmails@163.com>
2019-11-20 10:48:40 +0800

    删除多余的代码

--- src/components/list/index.js
+++
@@ -1,5 +0,0 @@
-import List from './List';
-
-List.install = Vue => Vue.component(List.name, List);
-
-export default List;
--- src/components/selectcheck/index.js
+++
@@ -1,5 +0,0 @@
-import Selectcheck from './Selectcheck';
-
-Selectcheck.install = Vue => Vue.component(Selectcheck.name, Selectcheck);
-
-export default Selectcheck;
