11ba473a4ac612d46261e24f2dd4a8ba7f8ad76a
xiongshuang <xsmails@163.com>
2019-12-30 15:06:33 +0800

    修改组件打包

--- src/entry/multiple.js
+++ src/entry/multiple.js
@@ -97,7 +97,7 @@ const install = Vue => Object.keys(Components).forEach(name => {
 });
 
 /* 支持使用标签的方式引入 */
-if (typeof window !== undefined && window.Vue) {
+if (typeof window !== 'undefined' && window.Vue) {
     install(window.Vue);
 }
 
