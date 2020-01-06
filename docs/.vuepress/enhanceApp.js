15409e114dee84ca0862af6173ed786db9733caf
xiongshuang <xsmails@163.com>
2020-01-06 17:37:20 +0800

    新增版本打印

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,5 +4,10 @@ import '../../dist/index.css';
 export default ({
     Vue
 }) => {
-    Vue.use(Pui)
+    Vue.use(Pui);
+    console.log(
+        '%c Persagy-ui %c Version 0.8.7 ',
+        'background-color: #0091ff; border-top-left-radius: 2px; border-bottom-left-radius: 2px; line-height: 20px; color: #fff; font-size: 14px;',
+        'background-color: #454d54; border-top-right-radius: 2px; border-bottom-right-radius: 2px; line-height: 20px; color: #fff; font-size: 14px;'
+    );
 }
