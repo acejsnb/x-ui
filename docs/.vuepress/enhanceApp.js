5df68430bccbf3b9cf048cb6f9ffabaeb743b6f1
xiongshuang <xsmails@163.com>
2020-02-03 13:44:41 +0800

    提交

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -6,14 +6,8 @@ export default ({
 }) => {
     Vue.use(Pui);
     console.log(
-<<<<<<< HEAD
         '%c Persagy-ui %c Version 0.8.9 ',
         'background-color: #0091ff; border-top-left-radius: 2px; border-bottom-left-radius: 2px; line-height: 20px; color: #fff; font-size: 12px;',
         'background-color: #454d54; border-top-right-radius: 2px; border-bottom-right-radius: 2px; line-height: 20px; color: #fff; font-size: 12px;'
-=======
-        '%c Persagy-ui %c Version 0.8.7 ',
-        'background-color: #0091ff; border-top-left-radius: 2px; border-bottom-left-radius: 2px; line-height: 20px; color: #fff; font-size: 14px;',
-        'background-color: #454d54; border-top-right-radius: 2px; border-bottom-right-radius: 2px; line-height: 20px; color: #fff; font-size: 14px;'
->>>>>>> a3b2f2f0a12b75d4e26629f0147a29cb762ffd47
     );
 }
